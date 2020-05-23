fc9a917a4686461ef350be4778ab1f37e568676a
brecwhite <59800248+brecwhite@users.noreply.github.com>
2020-05-22 21:54:24 -0400

    adds planning comments for future development

--- Payroll/main.cpp
+++ Payroll/main.cpp
@@ -4,11 +4,17 @@ using namespace std;
 
 int main() {
 
+//declare needed variables 
+/* Include first name, last name, employee id, amount paid, hours worked */
 
 
+//get user first name, last name, employee id, hours worked, amount paid
 
 
+//perform calculations for net and gross income
 
+
+//Print out what employee is getting paid
 
 
 	return 0;
