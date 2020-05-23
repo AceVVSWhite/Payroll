0fc0b8cbb9d1143a3eedb1e9e0ec3fda5d0e3864
brecwhite <59800248+brecwhite@users.noreply.github.com>
2020-05-23 15:44:36 -0400

    adds variable declarations

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	string  first_Name; //hold first name
+	string  last_Name; // hold last name
+	int employee_id;  //  hold employee id
+	double hourly_Rate;// hold rate of pay
+	double hours_Worked;//hold hours worked
+
+
+
+
+//get user first name, last name, employee id, hours worked, amount paid
+
+
+//perform calculations for net and gross income
+
+
+//Print out what employee is getting paid
+
+
+	return 0;
+}
